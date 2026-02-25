c5141666df3f3d0dbcccae0decc94ce6fad24415
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2026-02-25 12:00:08 +0530

    Implement match case for food ordering

---
+++ match case.py
@@ -0,0 +1,12 @@
+#MATCH CASE
+msg = '''
+Press 1 to Order Pizza
+Press 2 to Order Burger
+Press 3 to Order Dessert
+'''
+choice = int(input(msg))
+match choice:
+    case 1:print("Pizza Ordered Successfully...")
+    case 2:print("Burger Ordered Successfully...")
+    case 3:print("Dessert Ordered Successfully...")
+    case _:print("Invalid Choice")    
