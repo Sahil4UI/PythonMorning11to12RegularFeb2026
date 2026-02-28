7ca7db013198679ddbd2ffbd692360e8e966709c
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2026-02-28 13:06:40 +0530

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,10 @@
+x = int(input("Enter No1 : "))
+y = int(input("Enter No2 : "))
+z = int(input("Enter No3 : "))
+
+if x>y:
+    print(x,"is largest")
+elif x==y:
+    print("Both are Equal")
+else:
+    print(y,"is largest")
